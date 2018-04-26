50b12d463a2a4566f231e51a4f82f6cebbbfb7f5
BuckStopsElsewhere <33837622+BuckStopsElsewhere@users.noreply.github.com>
2018-04-25 22:02:57 -0400

    Updated with speedy ice

--- marbleGame.js
+++ marbleGame.js
@@ -115,26 +115,8 @@ The user moves a cube around the board trying to knock balls into a cone
 	function createLevel1() {
 		var light = createPointLight();
 		light.position.set(0,200,20);
-		var ground = createGround('bernie.jpg', 75, 100);
-		scene.add(ground);
-		var plane = createGroundFric('ice.jpg', 50, 200, 0);
-		plane.position.x = 0;
-		plane.position.y = 58;
-		plane.position.z = 130;
-		plane.rotateX(-Math.PI/5);
-		scene.add(plane);
-		var plane2 = createGround('bernie.jpg', 60, 150);
-		plane2.position.x = 0;
-		plane2.position.y = 120;
-		plane2.position.z = 295;
-		scene.add(plane2);
-		var cone = createConeMesh(5,20);
-		cone.position.set(0,120,295);
-		scene.add(cone);
-
-
 		// create the ground and the skybox
-		/*var ground = createGround('bernie.jpg', 40, 150);
+		var ground = createGround('bernie.jpg', 40, 150);
 		scene.add(ground);
 		var plane2 = createGround('bernie.jpg', 160, 30);
 		plane2.position.x = -60;
@@ -171,8 +153,6 @@ The user moves a cube around the board trying to knock balls into a cone
 		plane7.position.z = 25;
 		plane7.position.y = 0;
 		scene.add(plane7);
-		*/
-
 
 
 		var cone = createConeMesh(5,20);
@@ -218,8 +198,6 @@ The user moves a cube around the board trying to knock balls into a cone
 	}
 
 	function createLevel3() {
-		var light = createPointLight();
-		light.position.set(0,200,20);
 		var ground = createGround('bernie.jpg', 75, 100);
 		scene.add(ground);
 		var plane = createGroundFric('ice.jpg', 50, 200, 0);
@@ -229,7 +207,6 @@ The user moves a cube around the board trying to knock balls into a cone
 					controls.speed=140;
 				}
 			})
-
 		plane.position.x = 0;
 		plane.position.y = 58;
 		plane.position.z = 130;
@@ -238,8 +215,11 @@ The user moves a cube around the board trying to knock balls into a cone
 		var plane2 = createGround('bernie.jpg', 60, 150);
 		plane2.position.x = 0;
 		plane2.position.y = 120;
-		plane2.position.z = 300;
+		plane2.position.z = 295;
 		scene.add(plane2);
+		var cone = createConeMesh(5,20);
+		cone.position.set(0,120,295);
+		scene.add(cone);
 
 	}
 
