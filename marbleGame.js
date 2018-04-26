4bed0f77d6b7ad55056974e1099c16683bd599ed
BuckStopsElsewhere <33837622+BuckStopsElsewhere@users.noreply.github.com>
2018-04-25 21:51:15 -0400

    stuff

--- marbleGame.js
+++ marbleGame.js
@@ -113,8 +113,28 @@ The user moves a cube around the board trying to knock balls into a cone
 	}
 
 	function createLevel1() {
+		var light = createPointLight();
+		light.position.set(0,200,20);
+		var ground = createGround('bernie.jpg', 75, 100);
+		scene.add(ground);
+		var plane = createGroundFric('ice.jpg', 50, 200, 0);
+		plane.position.x = 0;
+		plane.position.y = 58;
+		plane.position.z = 130;
+		plane.rotateX(-Math.PI/5);
+		scene.add(plane);
+		var plane2 = createGround('bernie.jpg', 60, 150);
+		plane2.position.x = 0;
+		plane2.position.y = 120;
+		plane2.position.z = 295;
+		scene.add(plane2);
+		var cone = createConeMesh(5,20);
+		cone.position.set(0,120,295);
+		scene.add(cone);
+
+
 		// create the ground and the skybox
-		var ground = createGround('bernie.jpg', 40, 150);
+		/*var ground = createGround('bernie.jpg', 40, 150);
 		scene.add(ground);
 		var plane2 = createGround('bernie.jpg', 160, 30);
 		plane2.position.x = -60;
@@ -151,6 +171,7 @@ The user moves a cube around the board trying to knock balls into a cone
 		plane7.position.z = 25;
 		plane7.position.y = 0;
 		scene.add(plane7);
+		*/
 
 
 
@@ -203,13 +224,14 @@ The user moves a cube around the board trying to knock balls into a cone
 		scene.add(ground);
 		var plane = createGroundFric('ice.jpg', 50, 200, 0);
 		plane.position.x = 0;
-		plane.position.y = 55;
+		plane.position.y = 58;
 		plane.position.z = 130;
 		plane.rotateX(-Math.PI/5);
 		scene.add(plane);
 		var plane2 = createGround('bernie.jpg', 60, 150);
-		plane2.position.x = 60;
-		plane2.position.z = 150;
+		plane2.position.x = 0;
+		plane2.position.y = 120;
+		plane2.position.z = 300;
 		scene.add(plane2);
 
 	}
